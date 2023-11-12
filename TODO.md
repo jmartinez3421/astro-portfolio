d776f0188a1a7d68573b1f973a8df6e9b0d534ff
Fran <fran@ixorigue.com>
2023-11-12 19:28:22 +0100

    Contact section

---
+++ TODO.md
@@ -0,0 +1,8 @@
+### TODO List
+<hr>
+
+- [ ] Sección de contacto
+- [ ] Navbar para navegar entre secciones del Home (scroll) y para ir a Proyectos
+- [ ] Importar los proyectos en la página de Proyectos
+- [ ] Crear una especie de display de Blog con las mismas tarjetas que en Experience
+- [ ] Crear las páginas de los proyectos
--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -12,8 +12,11 @@ export const UI_I18N = {
         "header.description1": "I'm a Frontend developer with experience in React, NextJS, Angular, Typescript and Javascript. I've also worked in Backend using Node, express, Mongoose and a little bit of Java.",
         "header.description2": "I'm very creative and I like to bring a lot of ideas and improvements to the projects I work on.",
         "header.description3": "I am always looking for new challenges, and I am eager to collaborate with others on interesting projects. If you have any questions or would like to work together, please don't hesitate to contact me:",
+        "header.contact": "Get in touch",
         "experience.title": "My Experience",
         "experience.subtitle": "These are the companies I've worked for and the functions performed in each of them:",
+        "contact.title": "Contact me",
+        "contact.subtitle": "If you have any questions or would like to work together, please don't hesitate to contact me:",
     },
     es: {
         "header.title": "Hola, soy Jordi",
@@ -21,7 +24,10 @@ export const UI_I18N = {
         "header.description1": "Soy un desarrollador Frontend con experiencia en React, NextJS, Angular, Typescript y Javascript. También he trabajado en Backend usando Node, express, Mongoose y un poco de Java.",
         "header.description2": "Soy muy creativo y me gusta aportar muchas ideas y mejoras a los proyectos en los que trabajo.",
         "header.description3": "Siempre estoy buscando nuevos retos, y tengo muchas ganas de colaborar con otros en proyectos interesantes. Si tienes alguna pregunta o te gustaría trabajar juntos, no dudes en contactarme:",
+        "header.contact": "Contactar",
         "experience.title": "Mi Experiencia",
         "experience.subtitle": "Estos son las empresas para las que he trabajado y las funciones realizadas en cada una de ellas:",
+        "contact.title": "Contacta conmigo",
+        "contact.subtitle": "Si tienes alguna pregunta o te gustaría trabajar juntos, no dudes en contactarme:",
     }
 } as const;
